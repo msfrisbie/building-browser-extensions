bf0bb563918df68b12d908ac97cce2d860815e09
Matt Frisbie <msfrisbie@gmail.com>
2022-10-02 10:04:54 -0500

    links

--- pages/blog/manifest-v3-debacle-signals-a-changing-of-the-guard.js
+++ pages/blog/manifest-v3-debacle-signals-a-changing-of-the-guard.js
@@ -8,7 +8,7 @@ export default function BlogPost() {
         title="Manifest V3 Debacle Signals a Changing of the Guard | Matt Frisbie"
         url="https://www.buildingbrowserextensions.com/blog/manifest-v3-debacle-signals-a-changing-of-the-guard"
         description="The updated version is hostile both to developers and users, and
-        this shared adversity may prove to be consequential."
+        this shared adversity may prove to be a powerful catalyst."
       ></Header>
       <div className="flex flex-col items-center p-6 md:p-16">
         <div className="flex flex-col gap-8 font-light max-w-lg text-lg">
@@ -17,7 +17,7 @@ export default function BlogPost() {
           </div>
           <div className="text-xl">
             The updated version is hostile both to developers and users, and
-            this shared adversity may prove to be consequential.
+            this shared adversity may prove to be a powerful catalyst.
           </div>
           <div className="text-sm text-gray-500">10/3/2022</div>
           <hr></hr>
@@ -25,19 +25,21 @@ export default function BlogPost() {
             Following the initial manifest V3{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://blog.chromium.org/2018/10/trustworthy-chrome-extensions-by-default.html"
             >
               announcement
             </a>{" "}
-            in 2018, it did not take long for browser extension developers to
-            figure out that manifest V3 makes browser extensions less useful,
-            less powerful, and harder to build.{" "}
+            in 2018, browser extension developers did not take long to figure
+            out that manifest V3 makes browser extensions less useful, less
+            powerful, and harder to build.{" "}
           </div>
           <ul className="list-disc flex flex-col gap-4">
             <li>
               The blocking WebRequest API, which{" "}
               <a
                 target="_blank"
+                rel="noreferrer noopener"
                 href="https://adguard.com/en/blog/how-ad-blocking-is-done.html"
               >
                 all V2 ad blockers use
@@ -45,14 +47,21 @@ export default function BlogPost() {
               , is swapped out for an inferior DeclarativeNetRequest API.
             </li>
             <li>
-              Background pages, which allowed V2 extensions to control a
-              headless webpage that could persist indefinitely, are replaced by
-              background service workers that{" "}
+              V2 background pages — headless webpages that could persist
+              indefinitely — are replaced by service workers that{" "}
               <a
                 target="_blank"
+                rel="noreferrer noopener"
                 href="https://bugs.chromium.org/p/chromium/issues/detail?id=1152255"
               >
-                do not support persistent scripts
+                cannot persist indefinitely without{" "}
+                <a
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href="https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension/66618269#66618269"
+                >
+                  ugly hacks
+                </a>
               </a>
               .{" "}
             </li>
@@ -61,6 +70,7 @@ export default function BlogPost() {
               scripts, scripts loaded from remote URLs, or userscripts. (
               <a
                 target="_blank"
+                rel="noreferrer noopener"
                 href="https://github.com/Tampermonkey/tampermonkey/issues/644"
               >
                 Userscript extensions may still be saved.
@@ -74,6 +84,7 @@ export default function BlogPost() {
             up{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://gs.statcounter.com/browser-market-share/desktop/worldwide/2022"
             >
               over 80%
@@ -81,6 +92,7 @@ export default function BlogPost() {
             of worldwide desktop internet traffic, and with{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://www.debugbear.com/blog/counting-chrome-extensions"
             >
               180,000 extensions
@@ -92,24 +104,28 @@ export default function BlogPost() {
             with a bad{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://twitter.com/gorhill/status/1134127701583904770"
             >
               taste
             </a>{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://bugs.chromium.org/p/chromium/issues/detail?id=1152255"
             >
               in
             </a>{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://groups.google.com/a/chromium.org/g/chromium-extensions/c/xQmZLc8cu6Q"
             >
               its
             </a>{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://bugs.chromium.org/p/chromium/issues/detail?id=1135492"
             >
               mouth
@@ -118,6 +134,7 @@ export default function BlogPost() {
             efficacy of ad blockers. With Mozilla's recent{" "}
             <a
               target="_blank"
+              rel="noreferrer noopener"
               href="https://blog.mozilla.org/addons/2021/05/27/manifest-v3-update/"
             >
               announcement
@@ -128,7 +145,11 @@ export default function BlogPost() {
           <div>
             Meanwhile, the mobile extension landscape is shifting. In 2020,
             Apple{" "}
-            <a target="_blank" href="https://support.apple.com/en-us/HT212788">
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://support.apple.com/en-us/HT212788"
+            >
               added support
             </a>{" "}
             for browser extensions in iOS Safari. Though developers may balk at
